263526c932867f3d3d1e424cda67d38ed7e40c26
ISIKA2002 <isikapalei05.gmail.com>
2025-08-23 13:23:14 +0530

    Add

---
+++ product.py
@@ -0,0 +1,10 @@
+p_name= "Phone"
+p_quantity= 2
+p_price= 10000
+
+total_cost= p_quantity * p_price
+
+print("Product name:", p_name)
+print("Quantity:", p_quantity)
+print("Price:", p_price)
+print("Total price:", total_cost)
---
+++ variables.py
@@ -0,0 +1,25 @@
+#string_variable
+Name= "Isika"
+print(Name)
+print (type(Name))
+
+#integer_variable
+Age= 23
+print(Age)
+print(type(Age))
+
+#float-variable
+Score= 90.5
+print(Score)
+print(type(Score))
+
+#bloolean
+Is_employee= True
+print(Is_employee)
+print(type(Is_employee))
+
+#print_all_data
+print(Name, Age, Score, Is_employee)
+
+#print_all_in one line
+print("My name is " + Name + " and my age about ", Age ,"and", Is_employee , "as I am an employee and my score is", Score)
