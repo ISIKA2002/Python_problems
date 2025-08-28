71e4581f76f2c2fa15838389469d3a64d1ecd976
ISIKA2002 <isikapalei05.gmail.com>
2025-08-28 23:56:17 +0530

    Day3

---
+++ stringinput.py
@@ -0,0 +1,5 @@
+#- Build a program that asks for your name and favorite food, then prints a formatted sentence.
+your_name=str(input("Enter your name:"))
+your_fev_food=str(input("Enter your fav food:"))
+print("Your name is:", your_name)
+print("Your favorite food is:", your_fev_food)
