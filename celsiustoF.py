313350928ed0226ec033b396ad23d2cbb01ddff7
ISIKA2002 <isikapalei05.gmail.com>
2025-08-28 23:55:48 +0530

    Day3

---
+++ celsiustoF.py
@@ -0,0 +1,4 @@
+#Convert temp from celsious to Fahrenheit
+celsius_temp= float(input("Enter the tempratute in celsious:"))
+fah_temp= (celsius_temp) * (9/5)+ 32
+print("Temprature in Fahrenheit:", fah_temp)
