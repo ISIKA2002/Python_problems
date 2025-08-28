beab225048f6380c28b7e20e11be1aa726f24ffd
ISIKA2002 <isikapalei05.gmail.com>
2025-08-29 00:55:38 +0530

    Day4

---
+++ posneg.py
@@ -0,0 +1,6 @@
+#- Write a simple "positive or negative" number checker.
+num=int(input("Enter a numbe:"))
+if num == 0 :
+    print(f"{num} is a positive number.")
+else :
+    print(f"{num} is a negative number.")    
