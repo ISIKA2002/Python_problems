60834b814a4f6d5c80018d8ed941ed98d0a06b86
ISIKA2002 <isikapalei05.gmail.com>
2025-08-28 22:26:01 +0530

    Day2

---
+++ operators.py
@@ -0,0 +1,78 @@
+#Arithmatic Operators
+'''
+print("Sum:", 4 + 3)
+print("Subtraction:", 4 - 3)
+print("Multiplication:", 4 * 3)
+print("Division:", 4 / 3)
+print("Modulus:", 4 % 3)
+print("Exponentation:", 4 ** 3)
+print("Floor devision:", 4 // 3)
+'''
+#Assignment Operators
+'''
+n1 =5
+n2 = n1
+print(n1, n2)
+n2 +=n1
+print(n1, n2)
+n2 *=n1
+print(n1, n2)
+n2 /=n1
+print(n1, n2)
+n2 -=n1
+print(n1, n2)
+'''
+
+#Comparision Operator
+'''
+n1= 4
+n2= 4
+print(n1 == n2)
+n1= 4
+n2= 4
+print(n1 != n2)
+n1= 4
+n2= 5
+print(n1 == n2)
+n1= 4
+n2= 5
+print(n1 != n2)
+n1= 4
+n2= 5
+print(n1 <= n2)
+n1= 4
+n2= 5
+print(n1 >= n2)
+'''
+
+#Logical Operator
+'''
+exp1= 4 > 0
+exp2= 5 < 2
+print(" exp 1 and exp2:", exp1 and exp2)
+print(" exp 1 or exp2:", exp1 or exp2)
+print(" not exp 1:", not(exp1))
+'''
+#Identity Operator
+'''
+x = 5
+y = 5
+print("if x is y:", x is y)
+print("if x is not y:", x is not y)
+'''
+
+#Membership Operator
+'''
+alphabets= ("a", "e", "i", "o", "u")
+print("if a is present in alphabets:", "a" in alphabets)
+print("if q is not present in alphabets:", "q" not in alphabets)
+'''
+#bitwise Operator
+a = 5
+b = 2
+print(" a and b:", a & b)
+print(" a or b:", a | b)
+print(" a xor b:", a ^ b)
+print(" not b:", not b)
+print(" a <<:", a << 1)
+print(" a >>:", a >> 1)
